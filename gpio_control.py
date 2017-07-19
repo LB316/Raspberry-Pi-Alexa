d1a1df497e3b9e5a4facbfc11ce1f9584adf0414
LB316 <luke_beaulieu@student.uml.edu>
2017-07-19 17:11:55 +0000

    Update gpio_control.py

--- gpio_control.py
+++ gpio_control.py
@@ -3,12 +3,16 @@ from flask_ask import Ask, statement, convert_errors
 import RPi.GPIO as GPIO
 import logging
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@ask.launch
+def launch():
+    return statement('Ready to accept a command.')
 
 @ask.intent('GPIOControlIntent', mapping={'status': 'status', 'pin': 'pin'})
 def gpio_control(status, pin):
@@ -24,3 +28,23 @@ def gpio_control(status, pin):
     if status in ['off', 'low']:    GPIO.output(pinNum, GPIO.LOW)
 
     return statement('Turning pin {} {}'.format(pin, status))
+
+@ask.intent('LocationControlIntent', mapping={'status': 'status', 'location': 'location'})
+def location_control(status, location):
+
+    locationDict = {
+        'light': 11,
+	'led': 11
+    }
+
+    targetPin = locationDict[location]
+
+    GPIO.setup(targetPin, GPIO.OUT)
+
+    if status in ['on', 'high']:        GPIO.output(targetPin, GPIO.HIGH)
+    if status in ['off', 'low']:        GPIO.output(targetPin, GPIO.LOW)
+
+    return statement('Turning {} {}!'.format(location, status))
+if __name__ == '__main__':
+  port = 5000 #the custom port you want
+  app.run(host='0.0.0.0', port=port)
